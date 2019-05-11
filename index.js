0eb9fa4914985cbfaaea9ea5c0ce841f996b701c
lifecoder <lifecoder.ua@gmail.com>
2019-05-11 20:37:48 +0300

    replace koa with slack sdk

--- index.js
+++ index.js
@@ -3,13 +3,45 @@ const bodyParser = require('koa-bodyparser');
 
 const conf = require('./config');
 
-const app = new Koa();
+// Initialize using signing secret from environment variables
+const { createEventAdapter } = require('@slack/events-api');
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
 
-app.use(bodyParser());
+// Initialize using signing secret from environment variables
+const { createMessageAdapter } = require('@slack/interactive-messages');
+const slackInteractions = createMessageAdapter(process.env.SLACK_SIGNING_SECRET);
 
-app.use(async ctx => {
-  console.log(ctx.request.body);
-  ctx.body = ctx.request.body.challenge; //'Hello World';
+const port = process.env.PORT || 3000;
+
+// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+slackEvents.on('message', (event) => {
+  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
 });
 
-app.listen(3000);
+// Handle errors (see `errorCodes` export)
+slackEvents.on('error', console.error);
+
+// Start a basic HTTP server
+slackEvents.start(port).then(() => {
+  // Listening on path '/slack/events' by default
+  console.log(`server listening on port ${port}`);
+});
+
+
+const { WebClient } = require('@slack/web-api');
+
+// An access token (from your Slack app or custom integration - xoxp, xoxb)
+const token = process.env.SLACK_TOKEN;
+
+const web = new WebClient(token);
+
+// This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
+// const conversationId = 'C1232456';
+
+// (async () => {
+//   // See: https://api.slack.com/methods/chat.postMessage
+//   const res = await web.chat.postMessage({ channel: conversationId, text: 'Hello there' });
+
+//   // `res` contains information about the posted message
+//   console.log('Message sent: ', res.ts);
+// })();
